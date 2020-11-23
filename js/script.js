0f292b7bc5aaa1949768e12f9ded2f19c98fb55a
desarrollo <TKsDevelopment@Yusakus-MacBook-Pro.local>
2020-11-24 01:38:15 +0900

    changed

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// PhotoSwipe
+initPhotoSwipeFromDOM(".js-my-gallery");
+
+$(function () {
+
+  //iOS対策
+  //iOSでは疑似要素を含むaタグのリンクは２回タップしないと遷移とページ内スクロールをしないため、
+  //ユーザーエージェント判定でiOSの場合はbodyタグにiosを付与し、対象の疑似要素をdisplay: noneする
+  var ua = navigator.userAgent;
+  if (/iPhone|iPad|iPod/.test(ua)) {
+    $("body").addClass("ios");
+  }
+
+  //Worksのリンクを有効化
+  //スライド（Swiper）内に記載のリンクを有効にするため下記の記述が必要 (;´･ω･)ｳｰﾝ･･･
+  $(".works-url").on("click", "a", function (e) {
+    e.stopPropagation();
+  });
+
+  //ページ内スクロール
+  var $nav = $(".gnav");
+  var navHeight = $nav.outerHeight();
+
+  $('a[href^="#"]').on("click", function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - navHeight;
+    $("html, body").animate(
+      {
+        scrollTop: position,
+      },
+      300,
+      "swing"
+    );
+    return false;
+  });
+
+  //ページトップ
+  $("#js-page-top").on("click", function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      300
+    );
+    return false;
+  });
+});
